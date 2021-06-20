af0e5751d43e41325c08608241e12a1ee4969958
0xjrr <86163146+0xjrr@users.noreply.github.com>
2021-06-20 17:04:32 +0100

    Added nav & Section div

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const startButton = document.querySelector('.nav-start-button')
+const aboutButton = document.querySelector('.nav-about-button')
+const projectButton = document.querySelector('.nav-project-button')
+const contactButton = document.querySelector('.nav-contact-button')
+
+const startSection = document.querySelector('.nav-start-section')
+const aboutSection = document.querySelector('.nav-about-section')
+const projectSection = document.querySelector('.nav-project-section')
+const contactSection = document.querySelector('.nav-contact-section')
+
+
+
+startButton.addEventListener('click',function(){
+    startSection.classList.replace('invisible','visible')
+})
+aboutButton.addEventListener('click',function(){
+    aboutSection.classList.replace('invisible','visible')
+})
+projectButton.addEventListener('click',function(){
+    projectSection.classList.replace('invisible','visible')
+})
+contactButton.addEventListener('click',function(){
+    contactSection.classList.replace('invisible','visible')
+})
