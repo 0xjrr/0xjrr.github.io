1482d37fdb41c4da383636688ff7c3430408eb18
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-01-30 19:26:38 +0000

    create from v0

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { MainPage } from "@/components/main";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen min-w-full flex-col items-center ">
+      <MainPage>
+
+      </MainPage>
+    </main>
+  );
+}
