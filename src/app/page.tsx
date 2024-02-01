1adcdc0c34ca2c383e76ecab9f119fa20d8997fa
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-02-01 19:28:52 +0000

    clean main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import { MainPage } from "@/components/main";
+import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-full flex-col items-center ">
-      <MainPage>
-
-      </MainPage>
+      <MainPage />
     </main>
   );
 }
