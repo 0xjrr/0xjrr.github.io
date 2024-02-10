16c0033a976f56b3709d2c52335287c104148235
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-02-10 11:09:19 +0000

    resize modal

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -19,13 +19,13 @@ export default function ProjectModal({
       onClick={() => {
         setIsModalOpen(false);
       }}
-      className="fixed top-0 left-0 w-dvw h-dvh bg-gray-100 bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-50 flex items-center justify-center backdrop-blur-md"
+      className="fixed top-0 left-0 w-full h-full bg-gray-100 bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-50 flex items-center justify-center backdrop-blur-md"
     >
       <div
         onClick={(e) => {
           e.stopPropagation();
         }}
-        className="bg-white dark:bg-gray-800 p-6 rounded-xl w-3/4 max-h-screen-90 flex flex-col"
+        className="bg-white dark:bg-gray-800 p-4 sm:p-6 rounded-xl w-full sm:w-3/4 md:w-2/3 lg:w-2/3 xl:w-2/3 max-h-screen-90 flex flex-col"
       >
         <ModalContents
           className="overflow-auto scrollbar-thin flex-grow"
@@ -41,14 +41,14 @@ export default function ProjectModal({
             <div className="cursor-pointer flex items-center justify-center p-2 flex-grow">
               <div className="flex items-center hover:bg-gray-150 dark:hover:bg-gray-500 hover:shadow-lg bg-gray-200 dark:bg-gray-700 shadow-lg rounded-lg overflow-hidden max-w-lg flex-grow">
                 <div className="p-4 flex-grow">
-                  <p className="text-lg font-bold text-gray-500 dark:text-gray-300">
+                  <p className="text-sm md:text-lg font-bold text-gray-500 dark:text-gray-300">
                     View Github
                   </p>
                 </div>
 
                 {/* Icons */}
                 <div className="flex items-center space-x-4 p-4 bg-inherit justify-end">
-                  <IconGithub className="w-8 h-8" />
+                  <IconGithub className="w-6 h-6 md:w-8 md:h-8" />
                 </div>
               </div>
             </div>
@@ -63,7 +63,7 @@ export default function ProjectModal({
           >
             <div className="flex items-center hover:bg-red-300 dark:hover:bg-red-500 hover:shadow-lg bg-red-200 dark:bg-red-400 shadow-lg rounded-lg overflow-hidden max-w-lg w-full">
               <div className="p-4 flex-grow">
-                <p className="text-lg font-bold text-gray-500 dark:text-gray-200">
+                <p className="text-sm md:text-lg font-bold text-gray-500 dark:text-gray-200">
                   Close
                 </p>
               </div>
