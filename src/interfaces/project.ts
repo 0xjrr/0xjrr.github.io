db39068fa271a17c0dcd3f91c4b0a39fac76584f
0xjrr <86163146+0xjrr@users.noreply.github.com>
2024-06-09 19:08:40 +0100

    feat: Add download, videos, and images properties to Project interface

--- src/interfaces/project.ts
+++ src/interfaces/project.ts
@@ -2,6 +2,9 @@ interface Project {
     title: string;
     description: string;
     github: string;
+    download?: string;
+    videos?: string[];
+    images?: string[];
   }
 
 export default Project;
